944618195c8fd848bf8f115fa0352ccaf4ecf113
Your NameRobert <57042633+gaerlanrobert@users.noreply.github.com>
2020-02-11 15:12:12 -0800

    fixed javascript

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
+// saving input to local storage //
+
 function getLocalStorage(key) {
   let value = localStorage.getItem(key);
   if (value) {
     $(`#text${key}`).text(value);
   }
 }
-//  Creating the rows and columns //
+const newLocal = 1000;
+//  creating function using Moment JS documentation using Tokens //
 $(document).ready(function() {
   $("#todaysDate").text(moment().format(" Qo, h:m a, dddd, MMMM Do"));
   for (let i = 9; i < 18; i++) {
     var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+    // column 1 //
     var col1 = $(
       '<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + "</p>"
     );
+
+    // column 2 //
+
     var col2 = $(
       `<div class="col-sm-8 past"><textarea id=text${i} class="info" placeholder="input your task."></textarea>`
     );
+
+    // column 3 //
+
     var col3 = $(
       `<div class="col-sm-2"><button class="saveButton" id=${i}><i class="fas fa-save"></i></button>`
     );
+
+    // appending //
 
     row.append(col1);
     row.append(col2);
@@ -35,7 +48,7 @@ $(document).ready(function() {
     return hours + ampm;
   }
   formatAMPM();
-
+  // color change based on current time of day
   function changeColor() {
     var currentTime = new Date().getHours();
     for (var i = 9; i < 18; i++) {
@@ -49,8 +62,9 @@ $(document).ready(function() {
 
   setInterval(function() {
     changeColor();
-  }, 1000);
+  }, newLocal);
 
+  // saving input to local storage with onclick of button //
   var saveButton = $(".saveButton");
   saveButton.on("click", function() {
     let eventId = $(this).attr("id");
